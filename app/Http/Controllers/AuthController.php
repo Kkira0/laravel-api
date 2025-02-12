3911b6c055395f8de70266c50198208b1533ae32
pipisa <your@email.ex>
2025-02-12 13:59:57 +0200

    request validate authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function register(Request $request){
+        $request->validate([
+            'name'=> 'required|max:255',
+            'email'=> 'required|email|unique:users',
+            'password'=> 'required|confirmed',
+        ]);
         return 'register';
     }
 
