d93c941cb52c670633256c4213f623bd8c379e4a
pipisa <your@email.ex>
2025-02-12 13:54:58 +0200

    Route::post

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,6 @@ use App\Http\Controllers\PostController;
 
 Route::apiResource('posts', PostController::class);
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
