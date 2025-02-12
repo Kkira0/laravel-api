76f2f65270af6932960104e83d665ead915166c1
pipisa <your@email.ex>
2025-02-12 13:45:37 +0200

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,12 +24,13 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //
-        $request->validate([
+        $fields = $request->validate([
             'title' => 'required|max:255',
             'body' => 'required',
         ]);
 
-        return 'ok';
+        $post = Post::create($fields);
+        return $post;
     }
 
     /**
@@ -38,6 +39,7 @@ class PostController extends Controller
     public function show(Post $post)
     {
         //
+        return $post;
     }
 
     /**
@@ -46,6 +48,13 @@ class PostController extends Controller
     public function update(Request $request, Post $post)
     {
         //
+        $fields = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+
+        $post->update($fields);
+        return $post;
     }
 
     /**
